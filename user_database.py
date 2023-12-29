02a42f2009fce2a7acdb447de1fa51873c9c2132
Delarosa29 <edward.delaroesa@gmail.com>
2023-12-29 16:05:01 +1100

    Clears terminal screen when exiting application

--- user_database.py
+++ user_database.py
@@ -291,11 +291,15 @@ while True:
         # Displays all the existing contact entries 
         elif int_choice == 5:
             print("Showing Database Contents")
+            # Calls on show_contacts function and reads from json file
             show_contacts()
             continue
+        # Exits application
         elif int_choice == 6:
+            os.system("clear")
             print("Exit was selected; Goodbye!")
             break
     else:
+        # Will display if input is invalid and will ask again
         print(f"Invalid choice - {choice}; try again...")
         continue
