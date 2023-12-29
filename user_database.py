e813c7bc878f43e786c0d0c312b52d2d322c2daf
Delarosa29 <edward.delaroesa@gmail.com>
2023-12-29 11:24:05 +1100

    Import os and main menu styling added

--- user_database.py
+++ user_database.py
@@ -1,4 +1,10 @@
-print("Simple Database \n")
+import os
+
+# Use Case - Main Screen
+header = "Simple Database"
+print("-" * len(header))
+print(header)
+print("-" * len(header), "\n")
 
 print("1. Create new contact")
 print("2. Retrieve a contact by email")
