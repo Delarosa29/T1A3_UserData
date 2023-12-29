168e261cc14fe70ee7c65d0c7981becb02055917
Delarosa29 <edward.delaroesa@gmail.com>
2023-12-29 11:07:39 +1100

    Add menu

---
+++ user_database.py
@@ -0,0 +1,8 @@
+print("Simple Database \n")
+
+print("1. Create new contact")
+print("2. Retrieve a contact by email")
+print("3. Update a contact by email")
+print("4. Delete a contact by email")
+print("5. Show Database Contents")
+print("6. Exit\n")
